04b33ffff08641637ae7bc470e4aee68b6c0a61d
DsrknA <134616440+DsrknA@users.noreply.github.com>
2025-03-20 14:53:38 +0700

    Delete backend directory
    ganti directory

--- backend/src/test/java/com/ilyas/dessar/backend/exception/ProdukNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.ilyas.dessar.backend.exception;
-
-public class ProdukNotFoundException extends RuntimeException {
-    public ProdukNotFoundException(String message) { super(message); }
-}
--- backend/src/test/java/com/ilyas/dessar/backend/exception/ProdukValidationException.java
+++
@@ -1,5 +0,0 @@
-package com.ilyas.dessar.backend.exception;
-
-public class ProdukValidationException extends RuntimeException {
-    public ProdukValidationException(String message) { super(message); }
-}
